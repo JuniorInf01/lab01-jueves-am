4b8b4f5bceb9fe6ae4c11ec3499ff012eb7b13b6
Junior David Infantes Rondo <juniordavidinfantesrondo15@gmail.com>
2026-04-23 14:53:21 +0000

    fix(docker): Modificaciones en la web

--- iac/web.tf
+++ iac/web.tf
@@ -2,8 +2,14 @@ resource "docker_container" "web" {
   name  = "web-${terraform.workspace}"
   image = "lab/web"
 
+  networks_advanced {
+    name = docker_network.lab_net.name
+  }
+
   ports {
-    internal = "80"
+    internal = 80
     external = var.web_port[terraform.workspace]
   }
+
+  depends_on = [docker_container.api]
 }
